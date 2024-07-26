1678bd3c28cfa22ef7af6aeba16024b7c1c3a988
hyjoo <sparrow9576@gmail.com>
2024-07-26 16:55:49 +0900

    update_temp

--- 백준/단계별로 풀어보기/temp.py
+++ 백준/단계별로 풀어보기/temp.py
@@ -1,24 +1,29 @@
-N, M = map(int, input().split())
-A = []
-B = []
-C = []
+str_lst = []
+for i in range(5):
+    str_lst.append(input())
+print(str_lst)
 
-for i in range(N):
-    A.append(input().split())
-for i in range(N):
-    B.append(input().split())
+# N, M = map(int, input().split())
+# A = []
+# B = []
+# C = []
 
-for i in range(len(A)):
-    temp = []
-    for j in range(len(B)):
-        temp.append(int(A[i][j]) + int(B[i][j]))
-    C.append(temp)
+# for i in range(N):
+#     A.append(input().split())
+# for i in range(N):
+#     B.append(input().split())
+
+# for i in range(len(A)):
+#     temp = []
+#     for j in range(len(B)):
+#         temp.append(int(A[i][j]) + int(B[i][j]))
+#     C.append(temp)
 
 
-for i in C:
-    for j in range(len(i)):
-        if j == len(i) - 1:
-            print(i[j])
-        else:
-            print(i[j], end=' ')
-    print()
+# for i in C:
+#     for j in range(len(i)):
+#         if j == len(i) - 1:
+#             print(i[j])
+#         else:
+#             print(i[j], end=' ')
+#     print()
