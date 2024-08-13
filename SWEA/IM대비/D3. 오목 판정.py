b9dd7fbc8f1d7fb63e6221611bd3f77c6c1134f3
hyeonho.joo <sparrow9576@gmail.com>
2024-08-13 17:59:24 +0900

    임시저장

--- SWEA/IM대비/D3. 오목 판정.py
+++ SWEA/IM대비/D3. 오목 판정.py
@@ -8,9 +8,12 @@ for tc in range(1, T + 1):
     for i in range(N):
         for j in range(N):
             if arr[i][j] == 'o':    #내 위치에 돌이 있다면
-                for a in range(1, 5):   #자기 위치 + 최대 4까지
-                    di = [-a, a, 0, 0, -a, a, -a, a]  # 상하,좌우,대각,반대각 델타좌표
-                    dj = [0, 0, -a, a, a, -a, -a, a]
-
-                for b in range(8):
-                    arr[i + di[b]][j + dj[b]]
+                for k in range(4):
+                    count = 1
+                    for idx in range(1, 5): #전부 o면 오목
+                        di = [idx, 0, idx, idx]  # 하, 우, 대각아래(오, 왼) - 오른쪽아래로 순회해나가므로
+                        dj = [0, idx, idx, -idx]
+                        if arr[i + di[k]][j + dj[k]] != 'o'    #o가 아니면
+                            break
+                        else:   #o면
+                            count += 1
