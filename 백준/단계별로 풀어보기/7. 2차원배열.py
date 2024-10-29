8934f1b4287d4b4243284e00f4f9c476433c54ea
hyjoo1226 <sparrow9576@gmail.com>
2024-10-29 09:44:38 +0900

    일반수학1

--- 백준/단계별로 풀어보기/7. 2차원배열.py
+++ 백준/단계별로 풀어보기/7. 2차원배열.py
@@ -1,4 +1,18 @@
 #2738 행렬 덧셈
+n, m = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+b = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        a[i][j] += b[i][j]
+
+for i in range(n):
+    print(*a[i])
+# for i in range(n):
+#     for j in range(m):
+#         print(a[i][j], end=' ')
+#     print('')
 
 #2566 최댓값
 num_max = -1
@@ -15,5 +29,29 @@ print(num_max)
 print(target[0], target[1])
 
 #10798 세로읽기
+arr = [[] for _ in range(5)]
+
+for i in range(5):
+    arr[i] = list(input())
+    while len(arr[i]) < 15:
+        arr[i].append('@')
+
+for i in range(15):
+    for j in range(5):
+        if arr[j][i] != '@':
+            print(arr[j][i], end='')
 
 #2563 색종이
+arr = [[0] * 100 for _ in range(100)]
+n = int(input())
+for k in range(n):
+    a, b = map(int, input().split())
+    for i in range(b, b + 10):
+        for j in range(a, a + 10):
+            arr[i][j] = 1
+paper = 0
+for i in range(100):
+    for j in range(100):
+        if arr[i][j] == 1:
+            paper += 1
+print(paper)
--- 백준/단계별로 풀어보기/temp.py
+++ 백준/단계별로 풀어보기/temp.py
@@ -1,29 +1,11 @@
-str_lst = []
-for i in range(5):
-    str_lst.append(input())
-print(str_lst)
-
-# N, M = map(int, input().split())
-# A = []
-# B = []
-# C = []
-
-# for i in range(N):
-#     A.append(input().split())
-# for i in range(N):
-#     B.append(input().split())
-
-# for i in range(len(A)):
-#     temp = []
-#     for j in range(len(B)):
-#         temp.append(int(A[i][j]) + int(B[i][j]))
-#     C.append(temp)
-
-
-# for i in C:
-#     for j in range(len(i)):
-#         if j == len(i) - 1:
-#             print(i[j])
-#         else:
-#             print(i[j], end=' ')
-#     print()
+N, B = input().split()
+num = ''
+N = int(N)
+alp = {10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F', 16:'G', 17:'H', 18:'I', 19:'J', 20:'K', 21:'L', 22:'M', 23:'N', 24:'O', 25:'P', 26:'Q', 27:'R', 28:'S', 29:'T', 30:'U', 31:'V', 32:'W', 33:'X', 34:'Y', 35:'Z'}
+while N:
+    temp = N % int(B)
+    if temp >= 10:
+        temp = 
+    num = temp + num
+    N = N // int(B)
+print(num)
