dc5973f42a43f76306a91bc97ab6547a9fe68486
hyeonho.Joo <sparrow9576@gmail.com>
2026-07-22 16:52:46 +0900

    [Passed] 함수를 이용한 연속부분수열 여부 판단하기 (60ms, 16MB)

--- Codetree/trail2/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ Codetree/trail2/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -2,30 +2,45 @@ n1, n2 = map(int, input().split())
 n1_lst = list(map(int, input().split()))
 n2_lst = list(map(int, input().split()))
 
-if n2 > n1:
+def is_subsequence(n1_lst, n2_lst, n1, n2):
+    if n2 > n1:
+        return False
+        
+    for i in range(n1 - n2 + 1):
+        if n1_lst[i : i + n2] == n2_lst:
+            return True
+            
+    return False
+
+if is_subsequence(n1_lst, n2_lst, n1, n2):
+    print("Yes")
+else:
     print("No")
 
-elif n2 == n1:
-    if n1_lst[0] != n2_lst[0]:
-        print("No")
-    else:
-        print("Yes")
+# if n2 > n1:
+#     print("No")
 
-else:
-    pre_idx = []
-    for i in range(n1):
-        if n1_lst[i] == n2_lst[0] and i + n2 + 1 <= n1:
-            pre_idx.append(i)
+# elif n2 == n1:
+#     if n1_lst[0] != n2_lst[0]:
+#         print("No")
+#     else:
+#         print("Yes")
 
-    temp = []
-    for idx in pre_idx:
-        for i in range(n2):
-            if n2_lst[i] != n1_lst[idx + i]:
-                temp.append(i)
-                break
+# else:
+#     pre_idx = []
+#     for i in range(n1):
+#         if n1_lst[i] == n2_lst[0] and i + n2 + 1 <= n1:
+#             pre_idx.append(i)
 
-    if len(pre_idx) == len(temp):
-        print("No")
-    else:
-        print("Yes")
+#     temp = []
+#     for idx in pre_idx:
+#         for i in range(n2):
+#             if n2_lst[i] != n1_lst[idx + i]:
+#                 temp.append(i)
+#                 break
+
+#     if len(pre_idx) == len(temp):
+#         print("No")
+#     else:
+#         print("Yes")
         
